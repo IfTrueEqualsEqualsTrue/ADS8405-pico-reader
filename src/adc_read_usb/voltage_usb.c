5bf8e55fcf6253292ab6a63009428db82d43aafc
Guillaume <xgold3nfir3@gmail.com>
2025-04-29 16:08:42 +1000

    added simulator port

--- src/adc_read_usb/voltage_usb.c
+++ src/adc_read_usb/voltage_usb.c
@@ -2,10 +2,14 @@
 #include <stdio.h>
 #include "gpio_utils.h"
 
-
+#define SIMULATOR_PIN 26
 
 int main() {
     stdio_usb_init(); // Use the usb output as stdout 
+
+    gpio_init(SIMULATOR_PIN);
+    gpio_set_dir(SIMULATOR_PIN, GPIO_OUT);
+    gpio_put(SIMULATOR_PIN, 1);  // Always HIGH
 
     sleep_ms(2000); // 2 sec delay
     printf("Pico is ready !\n");
--- src/common/gpio_utils.c
+++ src/common/gpio_utils.c
@@ -43,7 +43,7 @@ float get_voltage(void) {
 }
 
 void spi_init_master(void) {
-    spi_init(SPI_PORT, 1000 * 1000); // 1 MHz
+    spi_init(SPI_PORT, 1 * 1000 * 1000); // 1 MHz
     gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
     gpio_set_function(PIN_CS, GPIO_FUNC_SPI);
     gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
